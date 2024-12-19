689374d32afbcc91a862faee74e2c0230052c649
pabitra <89703515+appu900@users.noreply.github.com>
2024-12-19 20:25:22 +0530

    added blocks

--- cmd/main.go
+++ cmd/main.go
@@ -1,10 +1,46 @@
 package main
 
-import "fmt"
+import (
+	"bufio"
+	"os"
+	"sync"
+	"time"
+)
 
-
-func main(){
-	fmt.Println("Hello World")
+type Record struct {
+	ID        string
+	Data      map[string]interface{}
+	CreatedAt time.Time
+	UpdatedAt time.Time
 }
 
+type Database struct {
+	mu      sync.RWMutex
+	records map[string]Record
+	indexes map[string]map[interface{}]string
+}
 
+// function for new Database Instance
+
+func NewDatabase() *Database {
+	return &Database{
+		records: make(map[string]Record),
+		indexes: make(map[string]map[interface{}]string),
+	}
+}
+
+type Terminal struct {
+	db     *Database
+	reader *bufio.Reader
+}
+
+func NewTerminal(db *Database) *Terminal {
+	return &Terminal{
+		db:     db,
+		reader: bufio.NewReader(os.Stdin),
+	}
+}
+
+func main() {
+
+}
